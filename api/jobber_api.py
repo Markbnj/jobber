4590afbfe8c09cff699cdde95fd6b649571b7b61
Mark Betz <betz.mark@gmail.com>
2015-12-29 01:09:18 -0500

    Implementing startup, logging, configuration.

--- api/jobber_api.py
+++ api/jobber_api.py
@@ -1,3 +1,5 @@
+import argparse
+import syslog
 from flask import Flask
 from redis import StrictRedis
 
@@ -8,5 +10,20 @@ def jobs(job=None):
     return "Get job descriptions here."
 
 if __name__ == "__main__":
-    rd = StrictRedis(host='localhost', port=6379)
-    app.run(host="0.0.0.0")
+    parser = argparse.ArgumentParser(description="jobber api server")
+    parser.add_argument("-p", "--port", help='The jobber server will listen on this port', default=5000)
+    parser.add_argument("-i", "--iface", help='The jobber server will bind to this interface', default="127.0.0.1")
+    args = parser.parse_args()
+    
+    syslog.openlog("jobber service", 0, syslog.LOG_LOCAL0)
+    syslog.syslog(syslog.LOG_INFO, "jobber service API starting on {}:{}".format(args.iface, args.port))
+
+    try:
+        rd = StrictRedis(host='localhost', port=6379)
+    except Exception as e:
+        syslog.syslog(syslog.LOG_ERR, "failed to create redis interface: {}".format(e))
+    else:
+        app.run(host=args.iface, port=args.port)
+    finally:
+        syslog.syslog(syslog.LOG_INFO, "jobber service API shutting down")
+        syslog.closelog()
