406828f6b9bf1586f878923b771c118242c83996
Mark Betz <betz.mark@gmail.com>
2015-12-30 00:41:50 -0500

    Added a cli arg to allow starting the server in debug mode.

--- api/jobber_api.py
+++ api/jobber_api.py
@@ -13,6 +13,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="jobber api server")
     parser.add_argument("-p", "--port", help='The jobber server will listen on this port', default=5000)
     parser.add_argument("-i", "--iface", help='The jobber server will bind to this interface', default="127.0.0.1")
+    parser.add_argument("-d", "--debug", help='Start the server in debug mode', action=store_true)
     args = parser.parse_args()
     
     syslog.openlog("jobber service", 0, syslog.LOG_LOCAL0)
@@ -23,6 +24,8 @@ if __name__ == "__main__":
     except Exception as e:
         syslog.syslog(syslog.LOG_ERR, "failed to create redis interface: {}".format(e))
     else:
+        if args.debug:
+            app.debug = True
         app.run(host=args.iface, port=args.port)
     finally:
         syslog.syslog(syslog.LOG_INFO, "jobber service API shutting down")
