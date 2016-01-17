0045fba611932a6f22d79a142ade76971928255a
Mark Betz <betz.mark@gmail.com>
2016-01-17 16:27:32 -0500

    Renamed the methods in the crontab module to be more meaningful.

--- jobber/api/crontabs.py
+++ jobber/api/crontabs.py
@@ -9,14 +9,17 @@
 """
 from crontab import CronTab
 
-def add_job(job):
+
+def add_job_crontab(job):
     # add the passed job to the crontab list
     pass
 
-def remove_job(job_id):
+
+def remove_job_crontab(job_id):
     # remove a job from the crontab list by id
     pass
 
-def sync_jobs(jobs):
+
+def sync_all_crontabs(jobs):
     # sync the crontab list with the passed job list
     pass
