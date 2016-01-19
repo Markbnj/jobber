bd4c98c140dad3aad091971c0008c14ec6a0599e
Mark Betz <betz.mark@gmail.com>
2016-01-19 00:28:56 -0500

    Fixed up the imports to follow a consistent style and adjusted variable names accordingly.

--- jobber/api/jobber_api.py
+++ jobber/api/jobber_api.py
@@ -10,12 +10,12 @@
 import argparse
 import syslog
 import json
-from flask import Flask, request, make_response
-from jobs import add_jobs, get_jobs, job_results, get_job, delete_job, update_job
-from api_error import BadRequestError, NotFoundError, InternalError
+import jobs
+import api_error
 import config
-from validator import get_swagger_spec
-from tests import get_postman_coll
+import validator
+import tests
+from flask import Flask, request, make_response
 
 
 app = Flask(__name__)
@@ -67,7 +67,7 @@ def get_swagger():
         Swagger spec (str):  jobber swagger spec as json doc.
     """
     try:
-        return _make_response(response=get_swagger_spec())
+        return _make_response(response=validator.get_swagger_spec())
     except Exception as e:
         return _make_error(500, e.message)
 
@@ -101,7 +101,7 @@ def get_postman():
         Postman collection (str):  postman collection as json doc.
     """
     try:
-        return _make_response(response=get_postman_coll())
+        return _make_response(response=tests.get_postman_coll())
     except Exception as e:
         return _make_error(500, e.message)
 
@@ -126,10 +126,10 @@ def get_post_jobs():
         Error (str):  json error if response status >=500
     """
     if request.method == 'POST':
-        jobs = request.json
+        jobs_data = request.json
         try:
-            return _make_response(response=add_jobs(jobs))
-        except BadRequestError as e:
+            return _make_response(response=jobs.add_jobs(jobs_data))
+        except api_error.BadRequestError as e:
             return _make_error(400, e.message)
         except Exception as e:
             return _make_error(500, e.message)
@@ -138,11 +138,11 @@ def get_post_jobs():
         item_count = request.args.get('item_count', None)
         name = request.args.get('name', None)
         try:
-            return _make_response(response=get_jobs(
+            return _make_response(response=jobs.get_jobs(
                     start_pos=start_pos,
                     item_count=item_count,
                     name=name))
-        except BadRequestError as e:
+        except api_error.BadRequestError as e:
             return _make_error(400, e.message)
         except Exception as e:
             return _make_error(500, e.message)
@@ -168,13 +168,13 @@ def get_jobs_results():
     start_pos = request.args.get('start_pos', None)
     item_count = request.args.get('item_count', None)
     try:
-        return _make_response(response=job_results(
+        return _make_response(response=jobs.job_results(
                 job_id=None,
                 start_time=start_time,
                 end_time=end_time,
                 start_pos=start_pos,
                 item_count=item_count))
-    except BadRequestError as e:
+    except api_error.BadRequestError as e:
         return _make_error(400, e.message)
     except Exception as e:
         return _make_error(500, e.message)
@@ -213,27 +213,27 @@ def get_put_delete_job(job_id):
     """
     if request.method == 'DELETE':
         try:
-            return _make_response(response=delete_job(job_id))
-        except NotFoundError as e:
+            return _make_response(response=jobs.delete_job(job_id))
+        except api_error.NotFoundError as e:
             return _make_error(404, e.message)
-        except BadRequestError as e:
+        except api_error.BadRequestError as e:
             return _make_error(400, e.message)
         except Exception as e:
             return _make_error(500, e.message)
     elif request.method == 'PUT':
         job = request.json
         try:
-            return _make_response(response=update_job(job_id, job))
-        except BadRequestError as e:
+            return _make_response(response=jobs.update_job(job_id, job))
+        except api_error.BadRequestError as e:
             return _make_error(400, e.message)
         except Exception as e:
             return _make_error(500, e.message)
     else:
         try:
-            return _make_response(response=get_job(job_id))
-        except NotFoundError as e:
+            return _make_response(response=jobs.get_job(job_id))
+        except api_error.NotFoundError as e:
             return _make_error(404, e.message)
-        except BadRequestError as e:
+        except api_error.BadRequestError as e:
             return _make_error(400, e.message)
         except Exception as e:
             return _make_error(500, e.message)
@@ -259,18 +259,24 @@ def get_job_results(job_id):
     start_pos = request.args.get('start_pos', None)
     item_count = request.args.get('item_count', None)
     try:
-        return _make_response(response=job_results(
+        return _make_response(response=jobs.job_results(
                 job_id=job_id,
                 start_time=start_time,
                 end_time=end_time,
                 start_pos=start_pos,
                 item_count=item_count))
-    except BadRequestError as e:
+    except api_error.BadRequestError as e:
         return _make_error(400, e.message)
     except Exception as e:
         return _make_error(500, e.message)
 
 
+""" API service process.
+
+Args:
+    -d:  run the server in debugging mode
+
+"""
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="jobber api service")
     parser.add_argument("-d", "--debug", help='Start the service in debug mode', action="store_true")
@@ -279,9 +285,6 @@ if __name__ == "__main__":
     interface = config.IFACE
     syslog.openlog("jobber api service", 0, syslog.LOG_LOCAL0)
     syslog.syslog(syslog.LOG_INFO, "jobber API service starting on {}:{}".format(interface, port))
-
-    # sync crontab at start
-
     if args.debug:
         app.debug = True
     app.run(host=interface, port=port)
